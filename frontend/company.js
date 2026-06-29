56d3a98c490d4bd55ac133ddc8b45b29252fdc2d
yashaachar <yashaachar15@gmail.com>
2026-06-29 16:43:48 +0530

    test git

---
+++ frontend/company.js
@@ -0,0 +1,2 @@
+export const name ="yasha";
+console.log(name);
---
+++ frontend/test.js
@@ -0,0 +1,54 @@
+//console.log("Hello World!");
+/*let x = 10;
+console.log(x);
+const y=20;
+console.log(y);
+console.log(x + y);*/
+//function add(x,y){
+ //   return x+y;
+//}
+//console.log(add(10,20));
+//const arr=[10,20,30,40,50];
+//console.log(arr[0]);
+//console.log(arr[1]);
+//console.log(arr[2]);
+//console.log(arr[3]);
+//console.log(arr[4]);
+
+//dictionary 
+//const dict = {"name":"John", "age":30, "city":"New York"};
+//console.log(dict.name);
+
+//destructuring
+//console.log(name);
+//console.log(age);
+//console.log(city);
+
+//destructuring of array
+//const arr=[10,20,30,40,50]
+//let [a,b,c,d,e]=arr;
+//console.log(a);
+//console.log(b);
+//console.log(c);
+//console.log(d);
+//console.log(e);
+
+//rest and spread operator
+//let a=[10,20];
+//let b=[30,40];
+//console.log(...a,...b);
+//console.log([...a,...b]);
+
+//rest operator --> ...(variable name)
+//function add(a,...b){
+ //   return a+b;
+//}
+//console.log(add(10,20));
+//console.log(add(10,20,30));
+//console.log(add(10,20,30,40));
+
+// Template literals
+//const a = 10;
+//const b = 20;
+//const sum = `The sum of ${a} and ${b} is ${a + b}`;
+//console.log(sum);
