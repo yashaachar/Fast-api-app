f1d11e9ac3a3d2a46bc3f2b292b1d3f0be46e744
yashaachar <yashaachar15@gmail.com>
2026-07-09 10:34:15 +0530

    Fix asyncpg dialect typo

--- backend/database.py
+++ backend/database.py
@@ -1,36 +1,27 @@
 import os
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine,async_sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL","postgresql://postgres:admin123@localhost:5432/student_db")
-
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:admin123@localhost:5432/student_db")
 
 if DATABASE_URL.startswith("postgres://"):
-    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql+async://", 1)
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql+asyncpg://", 1)
 elif DATABASE_URL.startswith("postgresql://"):
-    DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+async://", 1)
-
+    DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+asyncpg://", 1)
 
 if "supabase.com" in DATABASE_URL:
-    DATABASE_URL = DATABASE_URL.split("?")[0]  
-    engine = create_async_engine(DATABASE_URL, echo=False, connect_args={"ssl": "require"}) 
-
+    DATABASE_URL = DATABASE_URL.split("?")[0]
+    engine = create_async_engine(DATABASE_URL, echo=False, connect_args={"ssl": "require"})
 else:
     engine = create_async_engine(DATABASE_URL, echo=False)
-SessionLocal = async_sessionmaker(autocommit=False, autoflush=False, bind=engine,class_=AsyncSession)
+
+SessionLocal = async_sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
 Base = declarative_base()
 
 async def get_db():
     async with SessionLocal() as db:
-        try:
-            yield db
-        finally:
-            await db.close()
-    
-        
+        yield db
